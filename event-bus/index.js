e7c6160c432e849c3598b0270e6547e5eb0ffab3
Aaron Shack <aaronshack@Aarons-MacBook-Pro.local>
2023-08-05 15:44:46 -0700

    added initial node code

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,3 +1,13 @@
 /**
  * Event bus implementation
  */
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+
+const app = express();
+
+app.listen(4005, () => {
+  console.log("Listening on 4005");
+});
