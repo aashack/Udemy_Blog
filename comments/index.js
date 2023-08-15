79e5c23e24c815369c818ed675b7fda66709127b
Aaron Shack <aashack@gmail.com>
2023-08-15 11:28:42 -0700

    redo query because error

--- comments/index.js
+++ comments/index.js
@@ -20,7 +20,7 @@ app.post("/posts/:id/comments", async (req, res) => {
 
   const comments = commentsByPostId[req.params.id] || [];
 
-  comments.push({ id: commentId, content, status: 'pending' });
+  comments.push({ id: commentId, content, status: "pending" });
 
   commentsByPostId[req.params.id] = comments;
 
@@ -30,7 +30,7 @@ app.post("/posts/:id/comments", async (req, res) => {
       id: commentId,
       content,
       postId: req.params.id,
-      status: 'pending'
+      status: "pending",
     },
   });
 
@@ -38,28 +38,29 @@ app.post("/posts/:id/comments", async (req, res) => {
 });
 
 app.post("/events", async (req, res) => {
-  console.log("Event Received", req.body.type);
+  console.log("Event Received:", req.body.type);
 
-  const {type, data} = req.body;
+  const { type, data } = req.body;
 
-  if(type === 'CommentModerated') {
-    const { postId, id, status } = data;
-    const comments = [commentsByPostId];
-    const comment = comments.find(comment => {
+  if (type === "CommentModerated") {
+    const { postId, id, status, content } = data;
+    const comments = commentsByPostId[postId];
+
+    const comment = comments.find((comment) => {
       return comment.id === id;
     });
     comment.status = status;
-  }
 
-  await axios.post('http://localhost:4005', {
-    type: 'CommentUpdated',
-    data: {
-      id,
-      status,
-      postId,
-      content
-    }
-  })
+    await axios.post("http://localhost:4005/events", {
+      type: "CommentUpdated",
+      data: {
+        id,
+        status,
+        postId,
+        content,
+      },
+    });
+  }
 
   res.send({});
 });
--- query/index.js
+++ query/index.js
@@ -32,14 +32,15 @@ app.post('/events', (req, res) => {
     const { id, content, postId, status } = data;
 
     const post = posts[postId];
-
     const comment = post.comments.find(comment => {
-      return comment.id = id;
+      return comment.id === id;
     });
 
     comment.status = status;
     comment.content = content;
   }
+
+  console.log(posts);
 
   res.send({});
 });
